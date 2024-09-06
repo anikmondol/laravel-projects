4797000a99b71ccba3c69359cc3622fd52a355bf
anikmondol <anikmondol558363@gmail.com>
2024-09-07 02:47:24 +0600

    Creative-Writing login and register page done

--- Creative-Writing/resources/views/auth/register.blade.php
+++ Creative-Writing/resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -74,4 +74,85 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Register || creative-writing</title>
+    @vite('resources/css/app.css')
+
+    <link rel="shortcut icon" href="{{ asset('dashboard') }}/assets/images/neptune.png">
+
+    {{-- auth css file links --}}
+    <link href="{{ asset('dashboard') }}/assets/css/auth.css" rel="stylesheet" type="text/css">
+
+
+    <style>
+
+    </style>
+</head>
+<body>
+    <div class="relative flex flex-col justify-center overflow-hidden">
+        <div class="p-6 m-auto rounded-md shadow-2xl form-container">
+           <div class="flex items-center justify-center gap-2">
+            <h1 class="text-2xl font-semibold text-center text-gray-700">Creative-Writing</h1>
+            <img src="{{ asset('dashboard') }}/assets/images/neptune.png" alt="">
+           </div>
+            <form action="{{ route('register') }}" method="POST" class="space-y-2">
+                @csrf
+                <div>
+                    <label class="label">
+                        <span class="text-sm label-text">Name</span>
+                    </label>
+                    <input name="name" type="text" placeholder="Name" class="text-sm  input input-bordered @error('name') is-invalid @enderror" value="{{ old('name') }}"/>
+                    @error('name')
+                    <span class="text-red-600">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div>
+                    <label class="label">
+                        <span class="text-sm label-text">Email</span>
+                    </label>
+                    <input name="email" type="text" placeholder="Email Address" class="text-sm  input input-bordered @error('email') is-invalid @enderror" value="{{ old('email') }}"/>
+                    @error('email')
+                    <span class="text-red-600">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div>
+                    <label class="label">
+                        <span class="text-sm label-text">Password</span>
+                    </label>
+                    <input name="password" type="password" placeholder="Enter Password" class="text-sm  input input-bordered @error('password') is-invalid @enderror" value="{{ old('password') }}"/>
+                    @error('password')
+                    <span class="text-red-600">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="">
+                    <label class="label">
+                        <span class="text-sm label-text">Confirm Password</span>
+                    </label>
+                    <input name="password_confirmation" type="password" placeholder="Confirm Password" class="text-sm  input input-bordered @error('password_confirmation') is-invalid @enderror"/>
+                    @error('password_confirmation')
+                    <span class="text-red-600">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div>
+                    <button type="submit" class="btn hover:duration-700 btn-block mt-4 text-white">Register Account</button>
+                </div>
+            </form>
+        </div>
+        <div class="text-center mt-4">
+            <span class="">Already have an account ?
+                <a href="{{ route('login') }}" class=" text-blue-600 hover:text-blue-800 hover:underline">Login</a></span>
+        </div>
+    </div>
+</body>
+</html>
